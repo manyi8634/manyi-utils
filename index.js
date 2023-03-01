e921a5520af734462ead9df8d1fc316458a3ab07
manyi <863412757@qq.com>
2023-03-01 17:21:14 +0800

    js小数相减精度丢失减处理

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ export function isIE(){
   // const edge = userAgent.indexOf('Edge') > -1 && !ie  // 判断是否IE的Edge浏览器
   return ie || ie11
 }
+
+// js小数相减精度丢失减处理
+export function numSub(arg1,arg2){ 
+  let r1,r2,m,n; 
+  try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0} 
+  try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0} 
+  m=Math.pow(10,Math.max(r1,r2))
+  n=(r1>=r2)?r1:r2
+  return ((arg1*m-arg2*m)/m).toFixed(n); 
+}
