17b0022de4ac572945e1eecfa391bdde4e60b2f3
manyi <863412757@qq.com>
2023-05-19 13:31:46 +0800

    1.0.6发布

--- docs/config/api.md
+++ docs/config/api.md
@@ -2,19 +2,16 @@
 - manyi-utils集成了lodash并对其进行了扩展
 
 ```js
-import { deepClone } from 'manyi-utils'
-或
-import manyi from 'manyi-utils'
-
-const object = { a: undefined , b: ()=>{} ,c:...} 
-const deep = manyi.deepClone(object)
-
-
-支持lodash (version 4.17.21)用法，并对lodash进行了增强
 import _ from 'manyi-utils'
+或
+import { isIE , chunk } from 'manyi-utils'
+
+console.log(_.isIE()) // => true/false isIE为manyi特有
+console.log(_.chunk(['a','b','c','d'], 2)) // => [['a', 'b'], ['c', 'd']] chunk为lodash方法
+
 ```
 ## 关于lodash
-- manyi-utils集成了lodash,相关方法和lodash保持一致，使用方法请参考 [lodash官网](https://www.lodashjs.com)
+- manyi-utils集成了lodash，相关方法和lodash保持一致，目前依赖的lodash版本为`4.17.21`，后续lodash如有更新，我会看心情同步更新的~  使用文档请参考 [lodash官网](https://www.lodashjs.com)
 
 ## 关于manyi
 | 方法      | 说明          | 参数     | 返回值                           |
--- index.js
+++ index.js
@@ -58,10 +58,10 @@ export function isEmail(email) {
   return reg.test(email)
 }
 
-
 // 合并lodash,对lodash进行扩展
 import _ from 'lodash'
 
+// 默认导出
 export default {
   ..._,
   deepClone,
@@ -69,3 +69,94 @@ export default {
   numSub,
   isEmail
 }
+
+// 批量导出
+export {
+  // 数组
+  chunk, compact, concat, difference, differenceBy,
+  differenceWith, drop, dropRight, dropRightWhile, dropWhile,
+  fill, findIndex, findLastIndex, first, flatten,
+  flattenDeep, flattenDepth, fromPairs, head, indexOf,
+  initial, intersection, intersectionBy, intersectionWith, join,
+  last, lastIndexOf, nth, pull, pullAll,
+  pullAllBy, pullAllWith, pullAt, remove, reverse,
+  slice, sortedIndex, sortedIndexBy, sortedIndexOf, sortedLastIndex,
+  sortedLastIndexBy, sortedLastIndexOf, sortedUniq, sortedUniqBy, tail,
+  take, takeRight, takeRightWhile, takeWhile, union,
+  unionBy, unionWith, uniq, uniqBy, uniqWith,
+  unzip, unzipWith, without, xor, xorBy,
+  xorWith, zip, zipObject, zipObjectDeep, zipWith,
+
+  // 集合
+  countBy, each, eachRight, every, filter,
+  find, findLast, flatMap, flatMapDeep, flatMapDepth,
+  forEach, forEachRight, groupBy, includes, invokeMap,
+  keyBy, map, orderBy, partition, reduce,
+  reduceRight, reject, sample, sampleSize, shuffle,
+  size, some, sortBy, 
+
+  // 函数
+  after, ary, before, bind, bindKey,
+  curry, curryRight, debounce, defer, delay,
+  flip, memoize, negate, once, overArgs,
+  partial, partialRight, rearg, rest, spread,
+  throttle, unary, wrap, 
+
+  // 语言
+  castArray, clone, cloneDeep, cloneDeepWith, cloneWith,
+  conformsTo, eq, gt, gte, isArguments,
+  isArray, isArrayBuffer, isArrayLike, isArrayLikeObject, isBoolean,
+  isBuffer, isDate, isElement, isEmpty, isEqual,
+  isEqualWith, isError, isFinite, isFunction, isInteger,
+  isLength, isMap, isMatch, isMatchWith, isNaN,
+  isNative, isNil, isNull, isNumber, isObject,
+  isObjectLike, isPlainObject, isRegExp, isSafeInteger, isSet,
+  isString, isSymbol, isTypedArray, isUndefined, isWeakMap,
+  isWeakSet, lt, lte, toArray, toFinite,
+  toInteger, toLength, toNumber, toPlainObject, toSafeInteger,
+  toString,
+
+  // 数学
+  add, ceil, divide, floor, max,
+  maxBy, mean, meanBy, min, minBy,
+  multiply, round, subtract, sum, sumBy,
+
+  // 数字
+  clamp, inRange, random,
+
+  // 对象
+  assign, assignIn, assignInWith, assignWith, at,
+  create, defaults, defaultsDeep, entries, entriesIn,
+  extend, extendWith, findKey, findLastKey, forIn,
+  forInRight, forOwn, forOwnRight, functions, functionsIn,
+  get, has, hasIn, invert, invertBy,
+  invoke, keys, keysIn, mapKeys, mapValues,
+  merge, mergeWith, omit, omitBy, pick,
+  pickBy, result, set, setWith, toPairs,
+  toPairsIn, transform, unset, update, updateWith,
+  values, valuesIn,
+
+  // Seq
+  chain, tap, thru,
+
+  // 字符串
+  camelCase, capitalize, deburr, endsWith, escape,
+  escapeRegExp, kebabCase, lowerCase, lowerFirst, pad,
+  padEnd, padStart, parseInt, repeat, replace,
+  snakeCase, split, startCase, startsWith, template,
+  toLower, toUpper, trim, trimEnd, trimStart,
+  truncate, unescape, upperCase, upperFirst, words,
+
+  // 实用函数
+  attempt, bindAll, cond, conforms, constant,
+  defaultTo, flow, flowRight, identity, iteratee,
+  matches, matchesProperty, method, methodOf, mixin,
+  noConflict, noop, nthArg, over, overEvery,
+  overSome, property, propertyOf, range, rangeRight,
+  runInContext, stubArray, stubFalse, stubObject, stubString,
+  stubTrue, times, toPath, uniqueId, 
+
+  // Properties & Methods
+  VERSION, templateSettings
+
+} from 'lodash'
